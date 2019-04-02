49e9ca65cf97c08202ef702e73ba5289535c7aa9
Antonio de la Torre <antondelatorre@gmail.com>
2019-04-03 00:59:29 +0200

    Add Exercise1

---
+++ ejercicio1/src/test/java/Exercise1.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Exercise1 {
+
+    @Test
+    public void fix_me_and_rename_me(){
+        assertTrue(false);
+    }
+
+
+}
