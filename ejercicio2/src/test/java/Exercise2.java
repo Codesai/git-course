39bda18ba29cc4893012fd8a9802cf381aa40a71
Antonio de la Torre <antondelatorre@gmail.com>
2019-04-03 01:34:01 +0200

    Add ejercicio2

---
+++ ejercicio2/src/test/java/Exercise2.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Exercise2 {
+
+    @Test
+    public void fix_me_and_rename_me(){
+        assertTrue(true);
+    }
+
+
+}
